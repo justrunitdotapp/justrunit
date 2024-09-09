790d9ecdb4cb66a697ddb6ceff1b9cb66135278f
Wojciech Wozniak <inzynierz8@gmail.com>
2024-09-09 19:42:27 +0200

    updated error_tracker migration version to 3

--- priv/repo/migrations/20240825211454_add_error_tracker.exs
+++ priv/repo/migrations/20240825211454_add_error_tracker.exs
@@ -1,7 +1,7 @@
 defmodule MyApp.Repo.Migrations.AddErrorTracker do
   use Ecto.Migration
 
-  def up, do: ErrorTracker.Migration.up(version: 2)
+  def up, do: ErrorTracker.Migration.up(version: 3)
 
   # We specify `version: 1` in `down`, to ensure we remove all migrations.
   def down, do: ErrorTracker.Migration.down(version: 1)
